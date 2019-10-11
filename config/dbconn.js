b85898097c535ba05d767c96969ffd985c3aee46
Gharoro Pureheart <gharoropureheart@gmail.com>
2019-10-11 12:52:23 +0100

    modified database connection

--- config/dbconn.js
+++ config/dbconn.js
@@ -1,8 +1,8 @@
 /* eslint-disable comma-dangle */
 const Sequelize = require('sequelize');
 
-const db = new Sequelize('automart', 'postgres', 'jesus4ever', {
-  host: 'localhost',
+const db = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
+  host: process.env.DB_HOST,
   dialect: 'postgres',
   logging: false,
   pool: {
